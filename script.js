e0d08c27f7168841f684c5afc24e5e433e2ba9ae
sanjeevsingh22 <skrsingh876@gmail.com>
2024-05-28 23:11:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+// let heading = document.getElementsByClassName("heading");
+// console.log(heading);
+// console.dir(heading);
+// let parahs =document.getElementsByTagName("p");
+// console.dir(parahs);
+// console.log(parahs);
+
+// let div=document.querySelector("div");
+// console.dir(div);
+// let heading=document.querySelector("h1");
+
+// let h2=document.querySelector("h2");
+// console.dir(h2.innerText);
+// h2.innerText=h2.innerText+" from Apna College";
+// // console.log(h2);
+// let divs=document.querySelectorAll(".box");
+// console.log(divs[0]);
+// divs[0].innerText="new value of box 1";
+// divs[1].innerText="new value of box 2";
+// divs[2].innerText="new value of box 3";
+// better way of through box 
+
+// let idx=1;
+// for(div of divs){
+//     div.innerText=`new unique value of ${idx}`;
+//     idx++;
+// }
+
+// let para=document.querySelector("p");
+// // console.log(para.getAttribute("class"));
+// console.log(para.setAttribute("class", "newClass"));
+
+// let newBtn=document.createElement("button");
+// newBtn.innerText="click me!";
+// console.log(newBtn); 
+
+// let div=document.querySelector("div");
+// // div.append(newBtn);
+// // div.prepend(newBtn);
+// // div.before(newBtn);
+// // div.after(newBtn);
+// let p=document.querySelector("p");
+// p.after(newBtn);
+
+// let newHeading= document.createElement("h1");
+// newHeading.innerHTML="This is a new Heading";
+// document.querySelector("body").prepend(newHeading);
+
+// let para=document.querySelector("p");
+// para.remove();
+// newHeading.remove();
+
+// let newBtn=document.createElement("button");
+// newBtn.innerText="click me!";
+
+// newBtn.style.color="white";
+// newBtn.style.backgroundColor="red";
+// document.querySelector("body").prepend(newBtn);
+
+//Question2
+let para= document.querySelector("p");
+
+
