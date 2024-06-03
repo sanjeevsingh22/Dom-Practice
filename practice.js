0b4bee0d9284d57b90f9d306d60b4b52cf352414
sanjeevsingh22 <skrsingh876@gmail.com>
2024-06-03 23:35:54 +0530

    from new branch

--- practice.js
+++ practice.js
@@ -1,11 +1,33 @@
-let btn1=document.querySelector("#btn1");
-btn1.onclick=() =>{
-    console.log("btn1 was clicked");
-    let a=25;
-    a++;
-    console.log(a);
-}
-let box=document.querySelector("#box");
-box.onmouseover=() =>{
-    console.log("you are inside div");
-}
+ //let btn1=document.querySelector("#btn1");
+// btn1.onclick=(evt) =>{
+//     console.log(evt.type);
+//     console.log(evt.target);
+//     console.log(evt.clientX , evt.clientY);
+    // console.log("btn1 was clicked");
+    // let a=25;
+    // a++;
+    // console.log(a);
+// }
+// let box=document.querySelector("#box");
+// box.onmouseover=() =>{
+//     console.log("you are inside div");
+// }
+// btn1.addEventListener("click",(evt)=>{
+//     console.log("button1 was clicked");
+// })
+// let modeBtn= document.querySelector("#mode");
+// let currMode="light";//dark
+// let body =document.querySelector("body");
+// modeBtn.addEventListener("click", ()=>{
+//     if(currMode==="light"){
+//         currMode="dark";
+//         body.classList.add("dark");
+//         body.classList.remove("light");
+//     }
+// else{
+//     currMode="light";
+//     body.classList.add("light");
+//     body.classList.remove("dark");
+// }
+// console.log(currMode);
+// })
